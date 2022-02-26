c368aa89041c9c9179d815c90689575da98d6e3a
hc666666 <99161576+hc666666@users.noreply.github.com>
2022-02-26 15:02:00 +0800

    Add files via upload
    2015百度大小端面试题1分

---
+++ 2022.02.25.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>//判断大小端  代码存储从左往右 小端到大端 
+//char* *p 1个字节 int* *p 4个字节 
+//int main()
+//{
+//	int a=1;
+//	char* p=(char*)&a;
+//	if(*p=1)
+//	{
+//		printf("xiao");
+//	}
+//	else
+//	{
+//		printf("da");
+//	}
+//	
+//	return 0;
+// }
+//大小端 15年百度面试题 
+//int check_sys()
+//{
+//	int i=1;
+//	return (*(char*)&i);
+//}
+//int main()
+//{
+//	int ret=check_sys();
+//	if(ret=1)
+//	{
+//		printf("xiao");
+//		}	
+//	else
+//	{
+//		printf("da");
+//	}
+//}
+int main()//-1补码32个1 
+{
+	
+	return 0;
+ } 
